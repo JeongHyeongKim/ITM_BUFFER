3b29038d69462ca4bd37e0b8dd01dd6ae547e420
unknown <maladroit1@likelion.org>
2019-06-05 14:05:41 +0900

    fix_error_branch

--- src/main/java/com/gsitm/meeting/branch/dao/BranchDaoImpl.java
+++ src/main/java/com/gsitm/meeting/branch/dao/BranchDaoImpl.java
@@ -7,8 +7,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.gsitm.meeting.branch.dto.BranchDTO;
-import com.gsitm.meeting.room.dto.MeetingRoomDTO;
 import com.gsitm.meeting.vo.Branch;
 import com.gsitm.meeting.vo.MeetingRoom;
 
