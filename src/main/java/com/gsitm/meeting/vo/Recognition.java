91d66c6de69ed441fecb2cfeb8fe3d92af181a70
leehyobin <dlgyqls77@gmail.com>
2019-06-26 14:23:17 +0900

    recVo

--- src/main/java/com/gsitm/meeting/vo/Recognition.java
+++ src/main/java/com/gsitm/meeting/vo/Recognition.java
@@ -2,6 +2,13 @@ package com.gsitm.meeting.vo;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Recognition {
 	
 	private String recId;
@@ -11,46 +18,4 @@ public class Recognition {
 	private String resId;
 	private String recRefuse;
 	
-	public Recognition() {
-		super();
-	}
-	public String getRecId() {
-		return recId;
-	}
-	public void setRecId(String recId) {
-		this.recId = recId;
-	}
-	public String getDeptHeadId() {
-		return deptHeadId;
-	}
-	public void setDeptHeadId(String deptHeadId) {
-		this.deptHeadId = deptHeadId;
-	}
-	public String getManId() {
-		return manId;
-	}
-	public void setManId(String manId) {
-		this.manId = manId;
-	}
-	public String getRecAcceptDate() {
-		return recAcceptDate;
-	}
-	public void setRecAcceptDate(String recAcceptDate) {
-		this.recAcceptDate = recAcceptDate;
-	}
-	public String getResId() {
-		return resId;
-	}
-	public void setResId(String resId) {
-		this.resId = resId;
-	}
-	public String getRecRefuse() {
-		return recRefuse;
-	}
-	public void setRecRefuse(String recRefuse) {
-		this.recRefuse = recRefuse;
-	}
-	
-	
-
 }
